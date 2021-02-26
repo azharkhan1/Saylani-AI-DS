ae86eaf99059bb241b916b120073eac9307d087f
razalathiya <razalathiya@gmail.com>
2021-02-26 13:00:42 +0500

    done

--- Class 20 Django practice/sum/sum/views.py
+++ Class 20 Django practice/sum/sum/views.py
@@ -7,7 +7,20 @@ def index(request):
 def total(request):
     num1 = request.GET.get('num1')
     num2 = request.GET.get('num2')
-    total = int(num1) + int(num2)
+    operator = request.GET.get('operator')
+    total = 0
+    print(num1,num2,operator)
+    if (operator == '*'):
+        total = int(num1) * int(num2)
+    elif (operator == '+'):
+        total = int(num1) + int(num2)
+    elif (operator == '-'):
+        total = int(num1) - int(num2)
+    elif (operator == '/'):
+        total = int(num1) / int(num2)
+    else:
+        total = 'haha'
+        
     dic = {"total" : total}
     return render(request,'total.html' ,dic)
  
